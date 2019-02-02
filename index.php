bc99cbbce85f09d74c3c42f1f0da24327a64a0f3
deepsick <mav96@bk.ru>
2019-02-02 16:32:25 +0300

    Исправляет циклы и убирает пустые строки

--- index.php
+++ index.php
@@ -9,42 +9,36 @@ $ads = [
         'category' => 'Доски и лыжи',
         'price' => 10999,
         'url' => 'img/lot-1.jpg'
-
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
         'url' => 'img/lot-2.jpg'
-
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
         'url' => 'img/lot-3.jpg'
-
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
         'url' => 'img/lot-4.jpg'
-
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
         'url' => 'img/lot-5.jpg'
-
     ],
     [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
         'url' => 'img/lot-6.jpg'
-
     ]
 ];
 ?>
@@ -95,9 +89,9 @@ $ads = [
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <?php foreach ($categories as $key => $value): ?>
+            <?php foreach ($categories as $category): ?>
                 <li class="promo__item promo__item--boards">
-                    <a class="promo__link" href="pages/all-lots.html"><?=$value; ?></a>
+                    <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
@@ -107,18 +101,18 @@ $ads = [
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <?php foreach($ads as $key => $value): ?>
+            <?php foreach($ads as $ad): ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?=$value['url']; ?>" width="350" height="260" alt="">
+                        <img src="<?=$ad['url']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$value['category']; ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['title']; ?></a></h3>
+                        <span class="lot__category"><?=$ad['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ad['title']; ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=$value['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?=$ad['price']; ?><b class="rub">р</b></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
@@ -135,9 +129,9 @@ $ads = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <?php foreach ($categories as $key => $value): ?>
+            <?php foreach ($categories as $category): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?=$value; ?></a>
+                    <a href="pages/all-lots.html"><?=$category; ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
