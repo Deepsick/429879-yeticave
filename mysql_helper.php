3a4d5a4e71dce212490e0d53a6a89cc17f6fa2f3
deepsick <mav96@bk.ru>
2019-02-25 20:30:29 +0300

    Добавляет форму регистрации

--- mysql_helper.php
+++ mysql_helper.php
@@ -27,6 +27,9 @@ function db_get_prepare_stmt($link, $sql, $data = []) {
             }
             else if (is_double($value)) {
                 $type = 'd';
+            } 
+            else {
+                $type = 's';
             }
 
             if ($type) {
---
+++ sign-up.php
@@ -0,0 +1,57 @@
+<?php
+require_once 'db.php';
+require_once 'functions.php';
+
+$errors = [];
+$user = null;
+$categories = get_categories($connection);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $user = $_POST;
+    
+    $errors = validate_user_form();
+
+    if ($_FILES['avatar_url']['tmp_name'] !== '') {
+		$img_url = check_avatar();
+	} 
+    
+    if (check_user($connection)) {
+        $errors['user'] = 'Пользователь с этим email уже зарегистрирован';
+    }
+
+    if (check_username($connection)) {
+        $errors['name'] = 'Пользователь с таким именем уже зарегистрирован';
+    }
+
+    if (!count($errors)) {
+        $user_properties = 
+        [
+            'name' => $user['name'], 
+            'email' => $user['email'], 
+            'password' => $user['password'], 
+            'contacts' => $user['contacts'], 
+            'avatar_url' => $img_url
+        ];
+
+        $user_id = insert_user($connection, $user_properties);
+        if (is_null($user_id)) {
+            echo 'Ошибка сохранения в БД';
+            exit;
+        } 
+        else {
+            header("Location: /pages/login.html");
+        } 
+    }
+}
+
+$sign_up_page = include_template(
+    'sign-up.php',
+    [
+        'page_title' => 'Регистрация',
+        'categories' => $categories,
+        'errors' => $errors,
+        'user' => $user
+    ]
+);
+
+echo $sign_up_page;
